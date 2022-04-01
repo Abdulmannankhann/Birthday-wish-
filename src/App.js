e3ff87766375ac15f6bc0fa627c1614cbc3db8f6
Abdul Mannan KHAN <Abdul.Mannan.KHAN@soneparindia.com>
2022-04-01 22:18:24 +0530

    birthday wish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Confetti from "react-confetti";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Confetti />
+      <div className="card">
+        <div className="wish">
+          <h1>Happy Birthday</h1>
+        </div>
+        <div className="namee">
+          <h2>Mantasha Firdaus</h2>
+        </div>
+        <div className="body">
+          <p>
+            My childhood memories are full of laughter and fun because of you,
+            my dear big sister! I can't believe we're getting older and those
+            times seem like forever ago. <i class="fa-solid fa-heart"></i>
+          </p>
+        </div>
+        <div>
+          <h4 className="from">
+            <a
+              href="https://www.instagram.com/mannan.khan63/?hl=en"
+              target="__blank"
+            >
+              ~Mannan
+            </a>
+          </h4>
+        </div>
+      </div>
     </div>
   );
 }
